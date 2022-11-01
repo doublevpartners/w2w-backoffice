55867bd189d35a99e6567d76f0ca34fe52783fda
Nicolás Ríos <nicolas.rios@doublevpartners.com>
2022-11-01 17:41:15 -0500

    Initialized w2w project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,21 +15,21 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const fixture = TestBed.createComponent(AppComponent); // eslint-disable-line
+    const app = fixture.componentInstance; // eslint-disable-line
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'w2w'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const fixture = TestBed.createComponent(AppComponent); // eslint-disable-line
+    const app = fixture.componentInstance; // eslint-disable-line
     expect(app.title).toEqual('w2w');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppComponent); // eslint-disable-line
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
+    const compiled = fixture.nativeElement as HTMLElement; // eslint-disable-line
     expect(compiled.querySelector('.content span')?.textContent).toContain('w2w app is running!');
   });
 });
